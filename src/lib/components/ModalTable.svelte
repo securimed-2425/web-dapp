42fc02746abcb9c20e7418393f22304b3cc6c420
Angelo Convento <convento.angelo@gmail.com>
2024-06-13 22:16:45 +0800

    feat: Add ModalTable component

---
+++ src/lib/components/ModalTable.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+	import type { SvelteComponent } from 'svelte';
+
+	// Stores
+	import { getModalStore } from '@skeletonlabs/skeleton';
+
+	// Props
+	/** Exposes parent props to this component. */
+	export let parent: SvelteComponent;
+
+	const modalStore = getModalStore();
+
+	// Base Classes
+	const cBase = 'card p-4 w-modal shadow-xl space-y-4';
+	const cHeader = 'text-2xl font-bold';
+
+	let tableArr = [
+		{ firstName: 'Mikhaela', lastName: 'Tapia', alias: 'Kae' },
+		{ firstName: 'Alquen', lastName: 'Sarmiento', alias: 'Alquen' },
+		{ firstName: 'Angelo', lastName: 'Convento', alias: 'Gelo' }
+	];
+</script>
+
+<!-- @component This example creates a simple form modal. -->
+
+{#if $modalStore[0]}
+	<div class="modal-example-form {cBase}">
+		<header class={cHeader}>{$modalStore[0].title ?? '(title missing)'}</header>
+		<article>{$modalStore[0].body ?? '(body missing)'}</article>
+		<!-- Enable for debugging: -->
+
+		<!-- Responsive Container (recommended) -->
+		<div class="table-container">
+			<!-- Native Table Element -->
+			<table class="table table-hover">
+				<thead>
+					<tr>
+						<th>First Name</th>
+						<th>Last Name</th>
+						<th>Alias</th>
+					</tr>
+				</thead>
+				<tbody>
+					{#each tableArr as row, i}
+						<tr>
+							<td>{row.firstName}</td>
+							<td>{row.lastName}</td>
+							<td>{row.alias}</td>
+						</tr>
+					{/each}
+				</tbody>
+			</table>
+		</div>
+
+		<!-- prettier-ignore -->
+		<footer class="modal-footer {parent.regionFooter}">
+			<button class="btn {parent.buttonNeutral}" on:click={parent.onClose}>{parent.buttonTextCancel}</button>
+		</footer>
+	</div>
+{/if}
--- src/routes/[id]/settings/+page.svelte
+++ src/routes/[id]/settings/+page.svelte
@@ -1,17 +1,36 @@
 <script lang="ts">
-	import { initializeStores, Toast, getToastStore, type ToastSettings } from '@skeletonlabs/skeleton';
-	import type { PageData } from './$types';
+	import {
+		initializeStores,
+		Toast,
+		getToastStore,
+		Modal,
+		getModalStore,
+		type ToastSettings,
+		type ModalSettings,
+		type ModalComponent
+	} from '@skeletonlabs/skeleton';
 	import { user, db, getSCMRooms, getUserEPub } from '$lib/gun-setup';
+	import ModalTable from '$lib/components/ModalTable.svelte';
 	import SEA from 'gun/sea';
-
-	// export let data: PageData;
-
 	initializeStores();
+
+	const modalStore = getModalStore();
+	function viewTrustees(): void {
+		const c: ModalComponent = { ref: ModalTable };
+		const modal: ModalSettings = {
+			type: 'component',
+			component: c,
+			title: 'Trustees',
+			body: 'This contains the list of all your currently added trustees.',
+			response: (r) => console.log('response:', r)
+		};
+		modalStore.trigger(modal);
+	}
 
 	const toastStore = getToastStore();
 	let toastId = '';
 
-	const toastCreate = ( message: string ) => {
+	const toastCreate = (message: string) => {
 		const t: ToastSettings = {
 			message: message,
 			background: 'variant-filled-success',
@@ -19,50 +38,59 @@
 			timeout: 3000
 		};
 		toastId = toastStore.trigger(t);
-	}
+	};
 
 	const closeToast = () => {
 		toastStore.close(toastId);
-	}
-
-	let store : {[key: number]: string} = {};
-	let hr : number;
+	};
+	const cBase = 'card p-4 w-modal shadow-xl space-y-4';
+	let store: { [key: number]: string } = {};
+	let hr: number;
 	let firstname: string;
 	let lastname: string;
 	let saving: boolean = false;
 	let adding: boolean = false;
 	let trustees: { [key: string]: {} } = {};
 	let trusteeToAdd: string;
-	
-	user.get( 'securimed' ).get('profile').on( ( data: any, key: string ) => {
-		firstname = data.firstname || '';
-		lastname = data.lastname || '';
 
-		// console.log( 'securimed', data, key );
-	} );
+	user
+		.get('securimed')
+		.get('profile')
+		.on((data: any, key: string) => {
+			firstname = data.firstname || '';
+			lastname = data.lastname || '';
+
+			// console.log( 'securimed', data, key );
+		});
 	// user.get( 'securimed' ).get('recs').get('hr').map().on( async ( data: string, key: number ) => {
 	// 	const roompair = getSCMRooms();
 	// 	const val = await SEA.decrypt(data, roompair);
 	// 	store[key] = val;
 	// 	// console.log( 'data', key, data );
 	// } );
-	user.get( 'securimed' ).get('trs').map().on( async ( data: string, key: string ) => {
-		const trusteeEPub = getUserEPub( key );
-		const secret = await SEA.secret(trusteeEPub, user._.sea) || '';
-		if ( !secret ) {
-			console.log('cannot get trustee', key);
-			return;
-		}
-		
-		const dec = await SEA.decrypt(data, secret);
-		let alias = '';
-		db.user(key).get('alias').once( ( t_alias: string ) => {
-			console.log( 'alias', t_alias);
-			alias = t_alias;
+	user
+		.get('securimed')
+		.get('trs')
+		.map()
+		.on(async (data: string, key: string) => {
+			const trusteeEPub = getUserEPub(key);
+			const secret = (await SEA.secret(trusteeEPub, user._.sea)) || '';
+			if (!secret) {
+				console.log('cannot get trustee', key);
+				return;
+			}
+
+			const dec = await SEA.decrypt(data, secret);
+			let alias = '';
+			db.user(key)
+				.get('alias')
+				.once((t_alias: string) => {
+					console.log('alias', t_alias);
+					alias = t_alias;
+				});
+			trustees[key] = { alias, roompair: dec };
+			console.log('trustees', key, alias, dec);
 		});
-		trustees[key] = {alias, roompair: dec};
-		console.log( 'trustees', key, alias, dec );
-	} );
 
 	// const add = async () => {
 	// 	const datetime = new Date().valueOf();
@@ -75,87 +103,110 @@
 
 	const updateFirstName = () => {
 		saving = true;
-		user.get( 'securimed' ).get('profile').get('firstname').put( firstname ).then( () => {
-			toastCreate( 'First name updated!' );
-			saving = false;
-		});
-	}
+		user
+			.get('securimed')
+			.get('profile')
+			.get('firstname')
+			.put(firstname)
+			.then(() => {
+				toastCreate('First name updated!');
+				saving = false;
+			});
+	};
 
 	const updateLastName = () => {
 		saving = true;
-		user.get( 'securimed' ).get('profile').get('lastname').put( lastname ).then( () => {
-			toastCreate( 'Last name updated!' );
-			saving = false;
-		});
-	}
+		user
+			.get('securimed')
+			.get('profile')
+			.get('lastname')
+			.put(lastname)
+			.then(() => {
+				toastCreate('Last name updated!');
+				saving = false;
+			});
+	};
 
 	const addTrustee = async () => {
 		adding = true;
 		const data: { [key: string]: string } = {};
 		const roompair = getSCMRooms();
-		console.log( 'roompair', roompair );
+		console.log('roompair', roompair);
 		const trusteeEPub = getUserEPub(trusteeToAdd);
-		console.log( 'trusteeEPub', trusteeEPub );
-		const secret : string  = await SEA.secret( trusteeEPub, user._.sea ) || '';
-		if ( !secret ) {
-			toastCreate( 'Trustee not found!' );
+		console.log('trusteeEPub', trusteeEPub);
+		const secret: string = (await SEA.secret(trusteeEPub, user._.sea)) || '';
+		if (!secret) {
+			toastCreate('Trustee not found!');
 			adding = false;
 			return;
 		}
-		const enc = await SEA.encrypt( roompair, secret );
+		const enc = await SEA.encrypt(roompair, secret);
 		data[trusteeToAdd] = enc;
-		
-		user.get( 'securimed' ).get('trs').put( data ).then( () => {
-			toastCreate( 'Trustee granted access!' );
-			adding = false;
-		});
-	}
+
+		user
+			.get('securimed')
+			.get('trs')
+			.put(data)
+			.then(() => {
+				toastCreate('Trustee granted access!');
+				adding = false;
+			});
+	};
 
 	// $: heartrates = Object.entries(store).sort(( a: any, b: any ) => a[0] - b[0] );
-	$: _trustees = Object.entries(trustees).sort(( a: any, b: any ) => a[0] - b[0] );
-	
+	$: _trustees = Object.entries(trustees).sort((a: any, b: any) => a[0] - b[0]);
 </script>
 
 <Toast />
-
+<Modal />
 <div class="container h-full mx-auto flex justify-start items-start">
 	<div class="space-y-5">
 		<h2 class="h2">Settings</h2>
 		<div>
 			<h3 class="h3">Profile</h3>
 			{#if saving}
-			<p class="h6">Saving...</p>
+				<p class="h6">Saving...</p>
 			{/if}
 			<div class="ml-8 flex row gap-12">
 				<label class="label my-4">
 					<span>First Name</span>
-					<input class="input" type="text" placeholder="first name" bind:value={firstname} on:blur={updateFirstName}/>
+					<input
+						class="input"
+						type="text"
+						placeholder="first name"
+						bind:value={firstname}
+						on:blur={updateFirstName}
+					/>
 				</label>
 				<label class="label my-4">
 					<span>Last Name</span>
-					<input class="input" type="text" placeholder="last name" bind:value={lastname} on:blur={updateLastName}/>
+					<input
+						class="input"
+						type="text"
+						placeholder="last name"
+						bind:value={lastname}
+						on:blur={updateLastName}
+					/>
 				</label>
 			</div>
 			<h3 class="h3">Trustees</h3>
 			<div class="ml-8 my-4">
 				<div class="flex row gap-4 items-center pb-4 border-b border-b-white-200">
-					<p> View Trustees with access to your health records: </p>
+					<p>View Trustees with access to your health records:</p>
 					<!-- TODO: Add onclick function to view trustees' public key with option to remove -->
-					<button class="btn variant-filled-secondary" on:click={ () => console.log( 'trustees', _trustees ) } > View </button>
-					
+					<button class="btn variant-filled-secondary" on:click={viewTrustees}>View</button>
 				</div>
 				<label class="label my-4">
 					<span>Add Public Key of Trustee to Grant Access</span>
-					<input class="input" type="text" placeholder="Public Key" bind:value={trusteeToAdd}/>
+					<input class="input" type="text" placeholder="Public Key" bind:value={trusteeToAdd} />
 				</label>
 				{#if adding}
 					<p class="h6">Adding...</p>
 				{:else}
-					<button class="btn variant-filled-secondary" on:click={ addTrustee } > Add </button>
+					<button class="btn variant-filled-secondary" on:click={addTrustee}> Add </button>
 				{/if}
 			</div>
-			
-			
+
 			<!-- <label class="label">
 				<span>HR</span>
 				<input class="input" type="number" placeholder="first name" name="hr" bind:value={hr}/>
@@ -165,9 +216,6 @@
 			{#each heartrates as [key, value]}
 			<div>{key} : {value}</div>
 			{/each} -->
-			
-					
 		</div>
-		
 	</div>
 </div>
